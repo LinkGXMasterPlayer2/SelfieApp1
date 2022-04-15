053c971ff8a0b3b78817c0f7733d670bbac7d273
Link <95591923+LinkGXMasterPlayer2@users.noreply.github.com>
2022-04-14 17:35:33 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+function start(){
+
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+
+}
+recognition.onresult = function(event){
+
+    console.log(event);
+
+    var content = event.results[0][0].transcript;
+    console.log(content);
+
+    document.getElementById("textbox").innerHTML = content;
+
+    speak();
+
+}
+
+
+
+function speak(){
+
+    var synth = window.speechSynthesis;
+    speakdata = document.getElementById("textbox").value;
+    var utterthis = new SpeechSynthesisUtterance(speakdata);
+    synth.speak(utterthis);
+    Webcam.attach(camera);
+    
+}
+
+Webcam.set({
+
+    width:360,
+    height:250,
+    image_format:'png',
+    png_quality: 100
+});
+
+camera = document.getElementById("camera");
+
